1083459014ba7de5b59b20a01e4fbf04592a9cc8
Gabriel <gabrielpaixas@gmail.com>
2023-05-16 21:19:07 -0300

    .

---
+++ Q3/GerenciarNotas.java
@@ -0,0 +1,8 @@
+public class GerenciarNotas {
+
+    public void salvarNota(Pilha<Nota> notas, String nota) {
+        Nota novaNota = new Nota(nota);
+
+        notas.push(novaNota);
+    }
+}
---
+++ Q3/Nota.java
@@ -0,0 +1,15 @@
+public class Nota {
+    private String notas;
+
+    public Nota(String notas) {
+        this.notas = notas;
+    }
+
+    public String getNotas() {
+        return this.notas;
+    }
+
+    public void setNotas(String notas) {
+        this.notas = notas;
+    }
+}
